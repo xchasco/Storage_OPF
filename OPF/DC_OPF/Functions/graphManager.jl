8887de0dadb7dee34bf381f5348aae92591f7049
Josu <josutown@hotmail.es>
2025-03-07 11:44:50 +0100

    Working in a graphical implementation

--- OPF/DC_OPF/Functions/graphManager.jl
+++ OPF/DC_OPF/Functions/graphManager.jl
@@ -1,14 +1,27 @@
-using CairoMakie
+using PyPlot
 
 function graphManager(nN)
     # Determinar el tamaño de la malla (aproximadamente cuadrada)
     grid_size = ceil(Int, sqrt(nN))
 
     # Generar posiciones de los nodos en una cuadrícula
-    nodos = [(i, j) for i in 1:grid_size for j in 1:grid_size][1:nN]
+    all_nodes = [(i, j) for i in 1:grid_size for j in 1:grid_size]
+
+    # Asegurar que no exceda la cantidad de nodos disponibles
+    nN = min(nN, length(all_nodes))
+    nodos = all_nodes[1:nN]
+
+    # Extraer coordenadas X e Y
+    x_coords, y_coords = first.(nodos), last.(nodos)
 
     # Crear gráfico
-    fig, ax = scatter(first.(nodos), last.(nodos), markersize=10, color=:black)
+    fig, ax = subplots()
+    ax.scatter(x_coords, y_coords, s=50, c="black")  # s es el tamaño del marcador
 
-    display(fig)
+    # Quitar ejes y cuadrícula
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_frame_on(false)
+
+    show()
 end
